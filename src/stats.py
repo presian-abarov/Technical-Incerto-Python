57b8888cb747462db3285b6b0d52f3629356d9f3
xshi19 <soarshi@gmail.com>
2023-05-20 20:46:24 -0400

    Add Chapter 5

--- src/stats.py
+++ src/stats.py
@@ -1,9 +1,52 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+from scipy.fft import fft, ifft, fftshift
 from scipy.stats import powerlaw, norm, t, cauchy, pareto
 from sklearn.linear_model import LinearRegression, HuberRegressor
+
+def chf_to_pdf(chf, x_min=-10, x_max=10, n_points=1000):
+    """
+    Computes the PDF numerically from a given characteristic function using Fourier transformation.
+
+    Args:
+        char_func (function): Characteristic function of the distribution.
+        x_min (float, optional): Minimum value of x. Default is -10.
+        x_max (float, optional): Maximum value of x. Default is 10.
+        n_points (int, optional): Number of points to use in the discretization. Default is 1000.
+
+    Returns:
+        tuple: A tuple containing the x values (np.array) and the corresponding PDF values (np.array).
+    """
+
+    # Define the grid for x values
+    x = np.linspace(x_min, x_max, n_points)
+
+    # Define the grid for frequency values
+    freq = np.fft.fftfreq(n_points, d=x[1]-x[0]) * 2 * np.pi
+
+    # Evaluate the characteristic function on the frequency grid
+    chf_vals = chf(freq)
+    chf_vals[0] = 1
+
+    # Compute the inverse Fourier transform of the characteristic function values
+    pdf_vals = np.real(fftshift(ifft(chf_vals)))
+
+    # Normalize the PDF
+    pdf_vals /= (x[1] - x[0])
+
+    return x, pdf_vals
+
+def csch(x):
+    """
+    Compute the hyperbolic cosecant (csch) of x.
+    
+    Args:
+        x (float): The input value.
+        
+    Returns:
+        float: The hyperbolic cosecant of x.
+    """
+    return 1 / np.sinh(x)
 
 def detect_simple_regression_outliers(x, y, threshold=2.0):
     """
@@ -37,30 +80,6 @@ def detect_simple_regression_outliers(x, y, threshold=2.0):
     
     return outliers
 
-
-def compare_histograms(x, y, ax, bins=30, alpha=0.5):
-    """
-    Compare the histograms of two numpy arrays x and y with transparent bars.
-
-    Args:
-        x (array_like): First input array.
-        y (array_like): Second input array.
-        ax (matplotlib.axes.Axes): Matplotlib Axes object to plot the histograms.
-        bins (int): Number of bins for the histogram. Default is 30.
-        alpha (float): Transparency level for the histogram bars. Default is 0.5.
-
-    Returns:
-        None: Plots the histograms of x and y on the provided Axes object.
-    """
-    # Compute the range for both arrays
-    data_min = min(x.min(), y.min())
-    data_max = max(x.max(), y.max())
-
-    # Plot histograms
-    ax.hist(x, bins=bins, alpha=alpha, label='x', color='blue', range=(data_min, data_max))
-    ax.hist(y, bins=bins, alpha=alpha, label='y', color='red', range=(data_min, data_max))
-
-    ax.legend()
 
 def median_absolute_deviation(x, axis=None):
     """
@@ -225,3 +244,132 @@ def t_peak_shoulder_tail(mu, sigma, alpha):
             mu - x2 * sigma,
             mu + x2 * sigma,
             mu + x1 * sigma)
+
+def rousseeuw_croux_sn(x):
+    """
+    Compute the Sn scale estimator for the given x, as proposed by Rousseeuw and Croux (1993).
+    
+    Args:
+        x (array-like): A 1D NumPy array or a list of data points.
+    
+    Returns:
+        float: The Sn scale estimator for the input data.
+    
+    References:
+        Rousseeuw, P. J., & Croux, C. (1993). Alternatives to the Median Absolute Deviation.
+        Journal of the American Statistical Association, 88(424), 1273-1283.
+    """
+    x = np.asarray(x)
+    n = len(x)
+    abs_diffs = np.abs(x[:, None] - x)
+    medians = np.median(abs_diffs, axis=1)
+    sn = 1.1926 * np.median(medians)
+    return sn
+
+def rousseeuw_croux_qn(x):
+    """
+    Compute the Qn scale estimator for the given x, as proposed by Rousseeuw and Croux (1993),
+    with an updated scale coefficient from Akinshin (2022).
+    
+    Args:
+        x (array-like): A 1D NumPy array or a list of data points.
+    
+    Returns:
+        float: The Qn scale estimator for the input data.
+    
+    References:
+        Rousseeuw, P. J., & Croux, C. (1993). Alternatives to the Median Absolute Deviation.
+        Journal of the American Statistical Association, 88(424), 1273-1283.
+        
+        Akinshin, A. (2022). Finite-sample Rousseeuw-Croux scale estimators.
+    """
+    x = np.asarray(x)
+    n = len(x)
+    abs_diffs = np.abs(np.subtract.outer(x, x))
+    abs_diffs = abs_diffs[np.triu_indices(n, k=1)]
+    qn = 2.2191 * np.percentile(abs_diffs, 25)
+    return qn
+
+def empirical_survival_function(x):
+    """Computes the empirical survival function of the input data.
+
+    Args:
+        x (numpy array): Input data.
+
+    Returns:
+        tuple: Sorted input data and corresponding survival function values.
+    """
+    x = np.sort(x)
+    counts = np.arange(len(x), 0, -1)
+    sf = counts / len(x)
+    return x, sf
+
+
+class PowerLawEmpirical(object):
+    """Power law model for survival analysis.
+
+    The survival function is modeled as:
+    $$P(X>x) \sim C e^{-\alpha}$$
+    $$\log P(X>x) = \log C - \alpha \log(x)$$
+
+    Attributes:
+        min_size (int): Minimum size of the input data for the model to be fitted.
+    """
+    def __init__(self, min_size=100):
+        """Initializes the PowerLawEmpirical with the given minimum size.
+
+        Args:
+            min_size (int, optional): Minimum size of the input data for the model to be fitted. Defaults to 100.
+        """
+        self.min_size = min_size
+        
+    def fit(self, x):
+        """Fits the power law model to the input data.
+
+        Args:
+            x (numpy array): Input data.
+
+        Returns:
+            PowerLawEmpirical: The fitted model.
+        """
+        x, sf = empirical_survival_function(x[x>0])
+        self.x = x
+        self.sf = sf
+        
+        n = len(x)
+        logx = np.log(x)
+        logsf = np.log(sf)
+        
+        self.r2_adj = np.zeros(n-self.min_size)
+        for i in range(n-self.min_size):
+            lr = LinearRegression()
+            lr.fit(logx[i:].reshape(-1, 1), logsf[i:])
+            reg_line = lr.predict(logx[i:].reshape(-1, 1))
+            r2 = 1 - np.var(logsf[i:] - reg_line)/np.var(logsf[i:])
+            self.r2_adj[i] = 1 - ((1-r2)*(n-i-1)/(n-i-2))
+
+        self.iopt = np.argmax(self.r2_adj)
+        self.lr = LinearRegression()
+        self.lr.fit(logx[self.iopt:].reshape(-1, 1), logsf[self.iopt:])
+        self.c = np.exp(self.lr.intercept_)
+        self.alpha = -self.lr.coef_[0]
+        return self
+        
+    def loglog_plot(self, ax=None):
+        """Plots the survival function and the fitted power law model in log-log scale.
+
+        Args:
+            ax (matplotlib.axes.Axes, optional): The axes to draw the plot on. If not provided, a new figure and axes are created.
+        """
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(16, 9))
+            
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        
+        ax.scatter(self.x, self.sf, alpha=0.5)
+        ylim = np.array(ax.get_ylim())
+        ax.plot((ylim/self.c)**(-1/self.alpha), ylim, 'r', label="power law α = {0:.2}".format(self.alpha))
+        ax.set_ylim(ylim)
+        ax.grid(True, which="both", ls="-")
+        ax.legend()
